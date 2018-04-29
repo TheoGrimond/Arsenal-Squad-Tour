22814168dc806e764676204b0a5b23231d467c7d
tgrimond <tgrimond@gmail.com>
2018-04-29 15:51:21 -0400

    Added Player Details Component

--- Arsenal-Squad-Tour/angular-arsenal-tour/src/app/app.module.ts
+++ Arsenal-Squad-Tour/angular-arsenal-tour/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayersComponent
+    PlayersComponent,
+    PlayerDetailComponent
   ],
   imports: [
     BrowserModule,
